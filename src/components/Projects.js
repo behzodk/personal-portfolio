8fe49df4cc75ee13a89d4864c8372731b52ec482
Behzod <89260874+behzodk@users.noreply.github.com>
2023-01-11 19:23:19 +0500

    alt

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -92,7 +92,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" alt='' src={colorSharp2}></img>
     </section>
   )
 }
