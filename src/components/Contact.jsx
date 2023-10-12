75d32dad7e226a41c474156fc5af64500dc0c99b
Behzod <89260874+behzodk@users.noreply.github.com>
2023-10-12 22:22:36 +0500

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Behzod Musurmonqulov",
+          to_name: "Shohjahon Ilhomov",
           from_email: form.email,
-          to_email: "behzodmusurmonqulov@gmail.com",
+          to_email: "shohjahonilhomov4@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
